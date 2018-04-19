a4b7c44459f7e690213a095d895f4b30fdbe234c
wtscott@gmail.com <wtscott@gmail.com>
2018-04-19 20:35:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ function shout(string) {
   if(string === string.toLowerCase())
     { return "I can\'t hear you!"}
   else if(string === string.toUpperCase())
-    { return "YES, INDEED"}}
+    { return "YES, INDEED!"}
+  else if (string === "I love you, Grandma.")
+    {  return "I love you, too."}}
